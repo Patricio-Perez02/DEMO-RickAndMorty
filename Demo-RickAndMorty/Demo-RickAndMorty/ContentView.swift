bc7b3aca80b2f8c34c759c61d05a45765cd9e65e
Gonzalo <PJPerez@proveedor.teco.com.ar>
2023-10-31 15:56:31 -0300

    Initial commit.

---
+++ Demo-RickAndMorty/Demo-RickAndMorty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo-RickAndMorty
+//
+//  Created by Patricio Perez on 31/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo-RickAndMorty/Demo-RickAndMorty/Demo_RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_RickAndMortyApp.swift
+//  Demo-RickAndMorty
+//
+//  Created by Patricio Perez on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo_RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
