0434e660edc540b0ac78915e080b729a18edb80a
Gonzalo <PJPerez@proveedor.teco.com.ar>
2023-11-02 12:11:11 -0300

    AppIcon added, Pagination and Filter implemented.

--- Demo-RickAndMorty/Demo-RickAndMorty/Networking/Requests/Characters/CharacterService.swift
+++ Demo-RickAndMorty/Demo-RickAndMorty/Networking/Requests/Characters/CharacterService.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 protocol CharacterServiceProtocol {
-    func getCharacters() async throws -> [Character]
+    func getCharacters(with page: Int) async throws -> [Character]
 }
 
 class CharacterService: CharacterServiceProtocol {
     
     static let shared: CharacterServiceProtocol = CharacterService()
     
-    func getCharacters() async throws -> [Character] {
-        let response: CharactersResponse = try await APIManager.shared.request(with: .getCharacters)
+    func getCharacters(with page: Int) async throws -> [Character] {
+        let response: CharactersResponse = try await APIManager.shared.request(with: .getCharacters(id: page))
         return response.data ?? []
     }
 }
--- Demo-RickAndMorty/Demo-RickAndMorty/Screens/Characters/CharactersView.swift
+++ Demo-RickAndMorty/Demo-RickAndMorty/Screens/Characters/CharactersView.swift
@@ -13,42 +13,55 @@ struct CharactersView: View {
     private let columns = [ GridItem(.flexible()), GridItem(.flexible()) ]
     
     var body: some View {
-        ZStack(alignment: .top, content: {
-            
-            if let error = viewModel.error {
-                ErrorView(messages: error)
-            } else {
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 0, content: {
-                        ForEach(Array(viewModel.characters.enumerated()), id: \.element) { index, character in
-                            CharacterCell(character: character)
-                                .matchedGeometryEffect(id: character.name ?? "", in: animation)
-                                .onTapGesture {
-                                    withAnimation {
-                                        viewModel.selectedCharacter = character
-                                        viewModel.showCharacterDetail.toggle()
-                                    }
-                                }
-                        }
-                    })
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .scrollIndicators(.never)
+        NavigationStack {
+            ZStack(alignment: .top, content: {
                 
-                if viewModel.showCharacterDetail {
-                    CharacterDetailView(animation: animation)
-                        .environmentObject(viewModel)
+                if let error = viewModel.error {
+                    ErrorView(messages: error)
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 0, content: {
+                            ForEach(Array(viewModel.characters.enumerated()), id: \.element) { index, character in
+                                CharacterCell(character: character)
+                                    .matchedGeometryEffect(id: character.name ?? "", in: animation)
+                                    .onTapGesture {
+                                        withAnimation {
+                                            viewModel.selectedCharacter = character
+                                            viewModel.showCharacterDetail.toggle()
+                                        }
+                                    }
+                                    .onAppear {
+                                        loadMoreCharacters(with: character)
+                                    }
+                            }
+                        })
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .scrollIndicators(.never)
+                    
+                    if viewModel.showCharacterDetail {
+                        CharacterDetailView(animation: animation)
+                            .environmentObject(viewModel)
+                    }
                 }
+            })
+            .onAppear {
+                getCharacters()
             }
-        })
-        .onAppear {
-            getCharacters()
+            .navigationTitle("Characters")
         }
+        .searchable(text: $viewModel.searchText)
     }
     
     private func getCharacters() {
         Task {
             await viewModel.getCharacters()
+        }
+    }
+    
+    private func loadMoreCharacters(with currentItem: Character) {
+        Task {
+            await viewModel.loadMoreCharacters(currentItem: currentItem)
         }
     }
 }
